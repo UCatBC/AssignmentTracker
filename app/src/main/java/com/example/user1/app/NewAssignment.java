0caef0d212a556194427cbd1c5faed9e1f1d90b2
talhah1997 <talhah1997@hotmail.com>
2018-11-23 11:51:32 +0000

    Created drop down link once login

--- app/src/main/java/com/example/user1/app/NewAssignment.java
+++ app/src/main/java/com/example/user1/app/NewAssignment.java
@@ -2,6 +2,8 @@ package com.example.user1.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewAssignment extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class NewAssignment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_assignment);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
     }
 }
