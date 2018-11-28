fc2ac840aa01a4579e136f30cc462a39c17fff94
iantmc87 <iantmcmanus1@outlook.com>
2018-11-28 15:09:39 +0000

    Added Edit Expired Assignment page and updated navigation

--- app/src/main/java/com/example/user1/app/EditExpiredAssignment.java
+++ app/src/main/java/com/example/user1/app/EditExpiredAssignment.java
@@ -1,7 +1,10 @@
 package com.example.user1.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditExpiredAssignment extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class EditExpiredAssignment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_expired_assignment);
+    }
+
+    public void page1change(View View)
+    {
+        String button_text;
+        button_text =((Button)View).getText().toString();
+
+        if (button_text.equals("Create"))
+        {
+            Intent hello2 = new Intent(this,NewAssignment.class);
+            startActivity(hello2);
+        }
+        else if (button_text.equals("Current"))
+        {
+            Intent hello2 = new Intent(this,CurrentAssignments.class);
+            startActivity(hello2);
+        }
+        else if (button_text.equals("Expired"))
+        {
+            Intent hello2 = new Intent(this,ExpiredAssignments.class);
+            startActivity(hello2);
+        }
     }
 }
--- app/src/main/java/com/example/user1/app/ExpiredAssignments.java
+++ app/src/main/java/com/example/user1/app/ExpiredAssignments.java
@@ -5,6 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ExpiredAssignments extends AppCompatActivity {
 
@@ -12,6 +18,21 @@ public class ExpiredAssignments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expired_assignments);
+
+        SqliteHelper db = new SqliteHelper(this);
+        ArrayList<HashMap<String, String>> assignmentList = db.GetCurrentAssignments();
+        ListView lv = (ListView)findViewById(R.id.assignment_list);
+        ListAdapter adapter = new SimpleAdapter(ExpiredAssignments.this, assignmentList,
+                R.layout.list_row,new String[]{"title","deadline"},new int[]{R.id.title,R.id.deadline_date});
+        lv.setAdapter(adapter);
+    }
+
+    public void viewAssignment (View view) {
+        String button_text;
+        button_text = ((Button)view).getText().toString();
+
+        Intent assignment = new Intent(this, EditExpiredAssignment.class);
+        startActivity(assignment);
     }
 
     public void page1change(View View)
